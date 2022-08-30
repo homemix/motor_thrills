476d365207d8fcf8219e6c842985ecfef2853d32
homemix <kenmutati@gmail.com>
2022-08-30 18:04:38 +0300

    set up models

--- webapp/models.py
+++ webapp/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 
 class Brand(models.Model):
+    TRANSMISSION_TYPE = (
+        ('Auto', 'Automatic'),
+        ('Manual', 'Manual'),
+    )
+
     name = models.CharField('Name', max_length=100)
     model = models.CharField('Model', max_length=100)
     YOM = models.DateField('Year Of Make', blank=True, null=True)
     overview = models.TextField('Overview', blank=True, null=True)
     technical = models.TextField('Technical', blank=True, null=True)
     options = models.TextField('Options', blank=True, null=True)
+    transmission = models.CharField('Transmission', max_length=255, null=False,
+                                    choices=TRANSMISSION_TYPE, default='Auto')
     company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='company')
 
     def __str__(self):
@@ -33,7 +40,7 @@ class Vehicle(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='brand')
     Dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer')
     millage = models.IntegerField('Millage', )
-    price = models.IntegerField('Price',)
+    price = models.IntegerField('Price', )
 
     def __str__(self):
         return f'{self.brand} {self.Dealer} {self.millage} {self.price}'
