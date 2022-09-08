b9dc2bd3995af17f75fe53b9dee280cd7f333d5f
homemix <kenmutati@gmail.com>
2022-09-08 18:04:03 +0300

    ADD: filter cars by company.

--- webapp/views.py
+++ webapp/views.py
@@ -5,20 +5,16 @@ from .models import *
 
 def index(request):
     news = News.objects.all()
+    vehicle_make = Vehicle.objects.values_list('make', flat=True).distinct()
+    vehicle_YOM = Vehicle.objects.values_list('YOM', flat=True).distinct().order_by('-YOM')
     vehicles = Vehicle.objects.order_by('-YOM')[:8]
 
     return render(request, 'index.html', {
         'page_title': 'Home',
         'news': news,
         'vehicles': vehicles,
-    })
-
-
-def list_cars(request):
-    vehicles = Vehicle.objects.all()
-    return render(request, 'cars.html', {
-        'page_title': 'Cars',
-        'vehicles': vehicles
+        'vehicle_make': vehicle_make,
+        'vehicle_YOM': vehicle_YOM,
     })
 
 
@@ -29,22 +25,29 @@ class VehicleListView(ListView):
     context_object_name = 'vehicles'
     title = 'Cars'
     vehicle_make = Vehicle.objects.values_list('make', flat=True).distinct()
-    vehicle_YOM = Vehicle.objects.values_list('YOM', flat=True).distinct()
+    vehicle_YOM = Vehicle.objects.values_list('YOM', flat=True).distinct().order_by('-YOM')
+    vehicle_company= Company.objects.values_list('name', flat=True).distinct()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = self.title
         context['vehicle_make'] = self.vehicle_make
         context['vehicle_YOM'] = self.vehicle_YOM
+        context['vehicle_company'] = self.vehicle_company
         return context
 
     def get_queryset(self):
         make = self.request.GET.get('make')
         YOM = self.request.GET.get('YOM')
+        company = self.request.GET.get('company')
         if make:
-            return Vehicle.objects.filter(make__icontains=make)
+            return Vehicle.objects.filter(make__exact=make)
         elif YOM:
-            return Vehicle.objects.filter(YOM__icontains=YOM)
+            return Vehicle.objects.filter(YOM__exact=YOM)
+        elif company:
+            return Vehicle.objects.filter(company__name__exact=company)
+        if make and YOM:
+            return Vehicle.objects.filter(make__exact=make, YOM__exact=YOM)
         return Vehicle.objects.order_by('-YOM')
 
 
