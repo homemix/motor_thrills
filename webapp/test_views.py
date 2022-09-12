56a31ab87394ed19ced4e5521f7d73d9ec206b04
homemix <kenmutati@gmail.com>
2022-09-12 16:18:18 +0300

    ADD: user logs on review add

---
+++ webapp/test_views.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_about_us(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_contact_us(self):
+        self.fail()
